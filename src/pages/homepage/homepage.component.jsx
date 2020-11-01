d305ead5a94b9ab5627f383b64c9d3f3dbed6b83
applekiwiejuice <mcnikkopinlacnetflix@gmail.com>
2020-11-01 09:32:30 +0800

    updated our menu-items to navigate withRouter() HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => (
-  <div className="homepage">
-    <Directory />
-  </div>
-);
+const HomePage = (props) => {
+  console.log(props); // Prop Tunelling is a Bad Pattern
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
 
 export default HomePage;
